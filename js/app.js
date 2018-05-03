2f9b45b1e1135ab34dc1b78633ba859fc2810bce
Kenold Beauplan <bkenold@gmail.com>
2018-05-03 16:50:24 -0400

    remove handlebars

--- js/app.js
+++ js/app.js
@@ -1,26 +1,11 @@
-var ourRequest = new XMLHttpRequest();
-ourRequest.open('GET', 'js/shoes.json');
-ourRequest.onload = function () {
-    if (ourRequest.status >= 200 && ourRequest.status < 400) {
-        var data = JSON.parse(ourRequest.responseText);
-        createHTML(data);
-    } else {
-        console.log("We connected to the server, but it returned an error.");
+const app = new Vue({
+    el: '#app',
+    data: {
+        products: []
+    },
+    mounted() {
+        axios
+            .get('js/shoes.json')
+            .then(response => (this.products = response.data))
     }
-};
-
-ourRequest.onerror = function () {
-    console.log("Connection error");
-};
-
-ourRequest.send();
-
-
-function createHTML(petsData) {
-    var rawTemplate = document.getElementById("shoesTemplate").innerHTML;
-    var compiledTemplate = Handlebars.compile(rawTemplate);
-    var ourGeneratedHTML = compiledTemplate(petsData);
-
-    var petsContainer = document.getElementById("shoes-container");
-    petsContainer.innerHTML = ourGeneratedHTML;
-}
+})
